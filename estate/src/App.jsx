68a591c319e4cb762381626111c6c4aaabe58a07
nirajairi <nirajairi11@gmail.com>
2025-08-19 00:02:05 +0545

    About

--- estate/src/App.jsx
+++ estate/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import About from './components/About'
 function App() {
   return (<>
   
 <div  className='w-full overflow-hidden'>
     <Header/>
+    <About/>
 </div>
     {/* <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden' style={{backgroundImage: "url('/header_img.png')"}} id='Header'></div> */}
   </>
---
+++ estate/src/components/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='flex flex-col items-center justify-center container mx-auto px-14 mf:px-20 lg:px-32 w-full overflow-hidden' id='About'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2'>About <span className='underline underline-upset-4 decoration-1 under font-light'>Our brand</span></h1>
+      <p className='text-gray-500 max-w-80 text-center mb-8'> Passionate About properties , Dedicate to your vision</p>
+    </div>
+  )
+}
+
+export default About
